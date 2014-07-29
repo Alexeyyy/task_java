12f1012aee69fcc3c154b705bf23d0834cb2e677
Alex <a.zhelepov@yandex.ru>
2014-07-29 22:16:50 +0400

    Dictionary was figured out

---
+++ src/com/company/FileHandler.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Created by Alex on 29.07.2014.
+ */
+public class FileHandler {
+    public static void RetrieveDataToDictionary(String fPath, HashMap fMap) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fPath)))
+        {
+            String word = "";
+            while((word = reader.readLine()) != null)
+            {
+                fMap.put(word,word);
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Demanded dictionary file is not found!");
+        }
+    }
+
+    public static String LoadText(String fPath)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(fPath)))
+        {
+            String line ="";
+
+            while((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Demanded text file is not found!");
+        }
+
+        return sb.toString();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,28 +3,19 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+
+//1)Upload a dictionary to HashMap in java(+)
+//2)Upload text to String
 
 public class Main {
+    private static HashMap dictionary = new HashMap();
+    private static String text = "";
+
     public static void main(String[] args) {
-        System.out.print("Hello world from java!");
-        Worker.Out();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader("d://file_java.txt"))) {
-            String str = "";
-
-            System.out.print("\n");
-
-            while((str = reader.readLine()) != null) {
-                System.out.println(str);
-            }
-
-            System.out.print("\n");
-        }
-        catch (IOException e) {
-            System.out.print("I/O error: " + e);
-        }
-        finally {
-            System.out.print("Finally!");
-        }
+        FileHandler.RetrieveDataToDictionary("d:/file_java.txt", dictionary);
+        text = FileHandler.LoadText("d:/file_java.txt");
+        System.out.println(dictionary.size());
+        System.out.println(text);
     }
 }
